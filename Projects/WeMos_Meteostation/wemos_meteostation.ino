4b843a21c4d60203a93047fc481d24230a615a5f
Alex <alex.isayenko@gmail.com>
2019-04-10 13:32:07 +0300

    refactoring

--- Projects/WeMos_Meteostation/wemos_meteostation.ino
+++ Projects/WeMos_Meteostation/wemos_meteostation.ino
@@ -8,7 +8,6 @@
 #define mqtt_user "guest"      // if exist
 #define mqtt_password "guest"  //idem
 
-
 class RollingAverage{
   private:
     const int size = 50;
@@ -68,19 +67,18 @@ class RollingAverage{
 
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
-const float TOLERANCE = 0.5;
-
 RollingAverage rollingAverage;
+
+const float TOLERANCE = 0.3; // In Volts
+const int DELAY_TIME = 5; // Delay time in seconds for loop
 
 bool isValueChanged(float value1, float value2){
   return abs(value1 - value2) > TOLERANCE;
 }
 
-
 void setup() {
   Serial.begin(9600);     
   pinMode(LED_BUILTIN, OUTPUT);     //Pin 2 for LED
-  // digitalWrite(D2, HIGH);
   
   WiFi.begin(wifi_ssid, wifi_password);
   delay(10);
@@ -96,49 +94,72 @@ void setup() {
   // mqttClient.setCallback(callback);           // callback function to execute when a MQTT message   
 
   mqttClient.connect("ESP8266Client");
-    
 }
 
 int counter = 0;
 float previous_averageTemperature = -100;
-// bool isLedOn = false;
 
-void loop() {
-
+void blinkLed(){
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);    
+  delay(1000);      
+}
 
-if (mqttClient.state() != 0){
-
-  Serial.print("Trying to reconnect to mqtt server");
-  while (mqttClient.state() != 0){
-    Serial.print(".");
-    mqttClient.connect("ESP8266Client");    
-  }
+void checkConnectionAndReconnectIfNeeded(){
+    if (mqttClient.state() != 0){
   
-  Serial.println();
-  Serial.println("Reconnected");
-  }
+    Serial.print("Trying to reconnect to mqtt server");
+    while (mqttClient.state() != 0){
+      Serial.print(".");
+      mqttClient.connect("ESP8266Client");    
+    }
+    
+    Serial.println();
+    Serial.println("Reconnected");
+  }  
+}
+//
+//class Measurement{
+//
+//  private:
+//    const float INITIAL_VOLTAGE = 5.0;
+//    const float CORRECTION_COEFFICIENT = 0;
+//    
+//  public:
+//    float voltage;
+//    float temperature;
+//    
+//    Measurement(float analog_pin_raw_value){
+//      analog_pin_raw_value /= 1.65; // not sure why.
+//      voltage = INITIAL_VOLTAGE * analog_pin_raw_value / 1024;
+//      temperature = volgage / 0.01 - 273.14 + CORRECTION_COEFFICIENT;      
+//    }
+//};
 
-mqttClient.loop();
+const float INITIAL_VOLTAGE = 5.0;
+const float CORRECTION_COEFFICIENT = 0;
 
-
-  float initial_volts = 5.00;
-  int pin = A0;
-  float correction_coeficient = 0; //2.2;
-
-  float raw_val = (float) analogRead(pin) / 1.65;
+float getTemperature(float analog_pin_raw_value){
+  analog_pin_raw_value /= 1.65; // not sure why.
+  float voltage = INITIAL_VOLTAGE * analog_pin_raw_value / 1024;
+  float temperature = voltage / 0.01 - 273.14 + CORRECTION_COEFFICIENT;      
   
-  float volts = initial_volts * raw_val / 1024;
-  float celcius = volts / 0.01 - 273.14;
-  float current_temperature = celcius + correction_coeficient;
+  return temperature;
+}
+
+void loop() {
+
+  blinkLed();
+  
+  checkConnectionAndReconnectIfNeeded();
+  
+  mqttClient.loop();
+
+  float current_temperature = getTemperature ((float) analogRead(A0));
   rollingAverage.add(current_temperature);
 
   Serial.println("counter: " + String(counter++));
-  Serial.println("raw value: " + String(raw_val));
-  Serial.println("voltage: " + String(volts));
   Serial.println("cur temp: " + String(current_temperature));
   rollingAverage.serialPrint();
   float current_averageTemperature = rollingAverage.getAverage();
@@ -146,9 +167,6 @@ mqttClient.loop();
   Serial.println("rol avg: " + String(current_averageTemperature));
   Serial.println("mqtt state: " + String(mqttClient.state())); 
   Serial.println("wifi state: " + String(WiFi.status()));
-  Serial.println("-----");
-
-  
 
   if (isValueChanged(current_averageTemperature, previous_averageTemperature)){
     Serial.println("Value changed more than toleralnce level");
@@ -157,7 +175,8 @@ mqttClient.loop();
     mqttClient.publish("wemos", payload.c_str(), true);
   }
   
-  delay(5000);  
+  Serial.println("-----");
+  delay(DELAY_TIME*1000);  
 }
 
 
