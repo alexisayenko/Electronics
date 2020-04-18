84b7198f9e4576eb0bca6521b36e13df006cbb6f
Alex Isayenko <Alex.Isayenko@extraenergy.com.cy>
2020-04-18 20:52:23 +0300

    use relative path to libraries

--- Projects/wemos-lcd-http/arduino/lcd_http/lcd_http.ino
+++ Projects/wemos-lcd-http/arduino/lcd_http/lcd_http.ino
@@ -1,7 +1,7 @@
 
 #include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <LiquidCrystal_I2C.h>
+#include "ESP8266HTTPClient.h"
+#include "LiquidCrystal_I2C.h"
  
 LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x3F for a 16 chars and 2 line display
 const char* ssid = "netis";
