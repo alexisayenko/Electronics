c2b1d23608b11e319fb1bed309f4a603c6ab36ea
alex isayenko <alex.isayenko@gmail.com>
2023-11-28 21:00:21 +0200

    433

--- Displays/LCD 2004 12864/rpi4/hello-world.py
+++ Displays/LCD 2004 12864/rpi4/hello-world.py
@@ -1,18 +1,3 @@
-# Download LCD library
-# wget https://github.com/dbrgn/RPLCD/archive/refs/heads/master.zip
-
-# Unzip it and install
-# unzip master.zip
-# python RPLCD-master/setup.py install
-# rm -rf master.zip
-# rm -rf RPLCD-master
-
-# Turn on I2C interface
-# sudo raspi-config
-
-# connect display
-# SDA and SCL portsна on Raspberry Pi
-
 from RPLCD.i2c import CharLCD
 
 lcd = CharLCD('PCF8574', 0x27)  # Замените 0x27 на адрес вашего I2C, если он отличается
