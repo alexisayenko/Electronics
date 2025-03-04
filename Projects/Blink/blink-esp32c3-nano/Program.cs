45f4da1165d9b6fd294206665e425fbcba9bc2ad
alex isayenko <alex.isayenko@gmail.com>
2025-03-04 20:19:55 +0200

    add blink-esp32c3-nano

---
+++ Projects/Blink/blink-esp32c3-nano/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using nanoFramework.Hardware.Esp32;
+
+class Program
+{
+    static void Main()
+    {
+        int ledPin = 10;  // Adjust based on your board
+        GpioPin led = new GpioController().OpenPin(ledPin, PinMode.Output);
+
+        while (true)
+        {
+            led.Write(PinValue.High);
+            Thread.Sleep(500);
+            led.Write(PinValue.Low);
+            Thread.Sleep(500);
+        }
+    }
+}
